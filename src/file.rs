20ec158052fb064f179c8d54fb32c29077e2dcc0
egor-achkasov <eaachkasov@gmail.com>
2026-02-23 10:21:20 +0000

    Fix typos

--- src/file.rs
+++ src/file.rs
@@ -4,7 +4,7 @@ pub struct File {
     pub name_orig: String,
     /// timestampname, "17699100670710.mp4"
     pub name_timestamp: String,
-    /// thumbnail url, "https://archivach.vc/storage/t/aeaa7825f8d8ffe3f07f242a59b7761c.thumb"
+    /// thumbnail url, "https://arhivach.vc/storage/t/aeaa7825f8d8ffe3f07f242a59b7761c.thumb"
     pub url_thumb: String,
     /// url, "https://i.arhivach.vc/storage/a/ea/aeaa7825f8d8ffe3f07f242a59b7761c.mp4"
     pub url: String,
